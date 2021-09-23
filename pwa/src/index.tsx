90c22b32f6a48e736bfb2bec19bb841c2fc57e5f
Edazpotato <edazpotato@gmail.com>
2021-09-23 14:04:39 +1200

    serviceworkermaybe
    Signed-off-by: Edazpotato <edazpotato@gmail.com>

--- pwa/src/index.tsx
+++ pwa/src/index.tsx
@@ -1,5 +1,7 @@
 import "./index.css";
 
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+
 import App from "./App";
 import { CssBaseline } from "@mui/material";
 import React from "react";
@@ -21,3 +23,15 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals(console.log);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+window.serviceWorkerRegistration = serviceWorkerRegistration;
+
+declare global {
+	interface Window {
+		serviceWorkerRegistration: any;
+	}
+}
