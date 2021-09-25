66e8ccef9cbcafddfa29af75ae49d193e9a8932a
Edazpotato <edazpotato@gmail.com>
2021-09-25 17:58:22 +1200

    basic url aliases
    Signed-off-by: Edazpotato <edazpotato@gmail.com>

--- server/server.py
+++ server/server.py
@@ -1,4 +1,11 @@
+import sqlite3
 from fastapi import FastAPI,APIRouter
+from fastapi.responses import RedirectResponse
+
+not_sus_website = "https://wikipedia.org/"
+
+db = sqlite3.connect("datameridian.db")
+cursor = db.cursor()
 
 app = FastAPI()
 
@@ -8,8 +15,47 @@ api = APIRouter()
 async def get_api_root():
     return {"error": False, "data": {"message": "Hello, world!"}}
 
+@api.get("/url-aliases")
+async def get_url_aliases():
+    try:
+        aliases = []
+        for alias in cursor.execute("SELECT * FROM url_aliases ORDER BY created_at"):
+            # print(alias)
+            aliases.append({
+                "id": alias[0],
+                "slug": alias[1],
+                "cannoncial_url": alias[2],
+                "meta": {
+                    "title": alias[3],
+                    "description": alias[4],
+                    "colour": alias[5],
+                },
+                "uses": alias[6],
+                "created": alias[7]
+            })
+
+            return {"error": False, "data": aliases}
+
+    except Exception:
+        return {"error": True}
+
 app.include_router(api,prefix="/api")
 
 @app.get("/")
 def get_root():
     return "Wsssup?"
+
+@app.get("/a/{slug}")
+async def go_to_alias(slug: str):
+    try:
+        cursor.execute("SELECT * FROM url_aliases WHERE alias_slug=:slug", {"slug": slug})
+        alias = cursor.fetchone()
+        if (alias):
+            cursor.execute("UPDATE url_aliases SET uses=:updated_uses WHERE id=:id", {"id": alias[0], "updated_uses": alias[6]+1})
+            db.commit()
+            return RedirectResponse(alias[2])
+        else:
+            return RedirectResponse(not_sus_website)
+    except Exception as e:
+        print(e)
+        return RedirectResponse(not_sus_website)
---
+++ server/setup_database.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+db = sqlite3.connect("datameridian.db")
+cursor = db.cursor()
+
+cursor.execute("""
+CREATE TABLE "url_aliases" (
+	"id"	INTEGER NOT NULL UNIQUE,
+	"alias_slug"	TEXT NOT NULL UNIQUE,
+	"cannoncial_url"	TEXT NOT NULL,
+	"meta_title"	TEXT,
+	"meta_description"	TEXT,
+	"meta_colour"	TEXT,
+	"uses"	INTEGER NOT NULL,
+	"created_at"	INTEGER NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+""")
+db.commit()
+
+# for alias in cursor.execute("SELECT * FROM url_aliases ORDER BY created_at"):
+# 	print(alias)
+
+exit()
